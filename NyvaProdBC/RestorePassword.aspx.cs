ffcbb984bbf98b1cc1fa021e5ae71805dba9a2c0
HappyUnknown <vlad_dan@mail.ua>
2022-08-07 18:01:30 +0300

    Primary restoration functionality: non-aborting 30-minute session

--- NyvaProdBC/RestorePassword.aspx.cs
+++ NyvaProdBC/RestorePassword.aspx.cs
@@ -15,7 +15,7 @@ namespace NyvaProdBC
         {
             Response.Write("<script>alert('" + text + "')</script>");
         }
-        protected void Page_Load(object sender, EventArgs e)
+        int OpenedSessionIndex()
         {
             string key = Request.QueryString["restore_key"];
             var db = new PasswordRestoreSessionContext();
@@ -23,12 +23,39 @@ namespace NyvaProdBC
             ResponseAlert(DateTime.Parse(sessions[sessions.Count - 1].DateClosed) + ">" + DateTime.UtcNow.ToString());
             var openedPasswordSession = sessions.Where(x => x.GUID == key && DateTime.Parse(x.DateClosed) > DateTime.UtcNow).FirstOrDefault();
             //bool sessionOpened = DateTime.Parse(passwordSession.DateClosed) > DateTime.Now;
-            bool anyOpenedSession = openedPasswordSession != default;
-            KeyValid = anyOpenedSession;
+            int result = sessions.IndexOf(openedPasswordSession);
+            return result;
+        }
+        bool AnyOpenedSessionOnKey()
+        {
+            return OpenedSessionIndex() != -1;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            KeyValid = AnyOpenedSessionOnKey();
         }
 
         protected void btnRedeemRestore_Click(object sender, EventArgs e)
         {
+            int sessionIndex = OpenedSessionIndex();
+            if (sessionIndex != -1)
+            {
+                if (tbNewPassword.Text == tbNewConfirmation.Text)
+                {
+                    var sessionDB = new PasswordRestoreSessionContext();
+                    var sessions = sessionDB.PasswordSessions.ToList();
+                    var theSession = sessions[sessionIndex];
+                    var userDB = new NyvaUserContext();
+                    var users = userDB.Users.ToList();
+                    var theUser = users.Where(x => x.Id == theSession.UID).FirstOrDefault();
+                    int userIndex = users.IndexOf(theUser);
+                    users[userIndex].Password = tbNewPassword.Text;
+                    userDB.SaveChanges();
+                    Response.Redirect("/Login");
+                }
+                else ResponseAlert("Passwords do not match");
+            }
+            else ResponseAlert("No sessions available");
             //Restore password using key-user relation table for password request
             //If some user session passes for the request-key - redeem passsword change for this user (or just prevent form load if key does not exist)
         }
