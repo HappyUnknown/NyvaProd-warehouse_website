23bbf0d0e040d86c730e5672d493bb0c51e366f7
HappyUnknown <vlad_dan@mail.ua>
2022-08-09 11:45:15 +0300

    Admin goods: remove operation & handlers

--- NyvaProdBC/GoodDeletion.aspx
+++ NyvaProdBC/GoodDeletion.aspx
@@ -1,3 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GoodDeletion.aspx.cs" Inherits="NyvaProdBC.GoodDeletion" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label runat="server" ID="lblMsg" Text="Deletion page" />
+    <asp:Button runat="server" ID="btnRedeemDelete" OnClick="btnRedeemDelete_Click" />
 </asp:Content>
--- NyvaProdBC/GoodDeletion.aspx.cs
+++ NyvaProdBC/GoodDeletion.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NyvaProdBC.Entity;
+using NyvaProdBC.Entity.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,36 @@ namespace NyvaProdBC
 {
     public partial class GoodDeletion : System.Web.UI.Page
     {
+        static string goodID;
         protected void Page_Load(object sender, EventArgs e)
         {
+            goodID = Request.QueryString["id"];
+            if (!string.IsNullOrEmpty(goodID))
+            {
+                lblMsg.Text = $"Видалити товар із ID-{goodID}?";
+                btnRedeemDelete.Visible = true;
+            }
+            else
+            {
+                lblMsg.Text = "Як ви потрапили на цю сторінку? Поверніться до таблиці адміністрування, і повторіть спробу.";
+                btnRedeemDelete.Visible = false;
+            }
+        }
 
+        protected void btnRedeemDelete_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(goodID))
+            {
+                string goodID = Request.QueryString["id"];
+                var db = new GoodContext();
+                var goods = db.Goods.ToList();
+                Good theGood = goods.Where(x => x.Id == int.Parse(goodID)).FirstOrDefault();
+                int removeIndex = goods.IndexOf(theGood);
+                goods.RemoveAt(removeIndex);
+                db.SaveChanges();
+                Response.Redirect("/AdminGoods");
+            }
+            else Response.Write($"<script>alert('Неможливо видалити: обраний елемент не існує');</script>");
         }
     }
 }
