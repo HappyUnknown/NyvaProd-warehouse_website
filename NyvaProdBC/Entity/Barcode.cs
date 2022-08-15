1a95d46b80f2ad52780698010e0ec60bddec030f
HappyUnknown <vlad_dan@mail.ua>
2022-08-15 18:20:24 +0300

    Barcode string normalized

--- NyvaProdBC/Entity/Barcode.cs
+++ NyvaProdBC/Entity/Barcode.cs
@@ -21,7 +21,43 @@ namespace NyvaProdBC.Entity
         }
         public override string ToString()
         {
-            return RegionCode + "'" + ProducerCode + "'" + GoodCode + "'" + ControlDigit;
+            int regCodeDigits = 3;
+            int regCodeDigitCount = regCodeDigits - 1;//hardcode, because I do not understand
+            int regCode = RegionCode;
+            string strRegCode = regCode.ToString();
+            while (regCode > 1)
+            {
+                regCode /= 10;
+                regCodeDigitCount--;
+            }
+            for (int i = 0; i < regCodeDigitCount; i++)
+                strRegCode = "0" + strRegCode;
+
+            int prodCodeDigits = 4;
+            int prodCodeDigitCount = prodCodeDigits - 1;//hardcode, because I do not understand
+            int prodCode = ProducerCode;
+            string strProdCode = prodCode.ToString();
+            while (prodCode > 1)
+            {
+                prodCode /= 10;
+                prodCodeDigitCount--;
+            }
+            for (int i = 0; i < prodCodeDigitCount; i++)
+                strProdCode = "0" + strProdCode;
+
+            int goodCodeDigits = 5;
+            int goodCodeDigitCount = goodCodeDigits - 1;//hardcode, because I do not understand
+            int goodCode = GoodCode;
+            string strGoodCode = goodCode.ToString();
+            while (goodCode > 1)
+            {
+                goodCode /= 10;
+                goodCodeDigitCount--;
+            }
+            for (int i = 0; i < goodCodeDigitCount; i++)
+                strGoodCode = "0" + strGoodCode;
+
+            return strRegCode + "'" + strProdCode + "'" + strGoodCode + "'" + ControlDigit;
         }
     }
 }
